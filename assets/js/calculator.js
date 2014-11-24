2514dc1f5dcf2fa7c8dc499d287e97198d59f092
axocomm <axocomm@gmail.com>
2014-11-24 11:48:39 -0500

    Handle current form submit and add formula

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -1,15 +1,75 @@
 'use strict';
 
-$(document).ready(function() {
+$(document).ready(function () {
     var ex = {
-        loanAmount: 100000,
-        monthlyInterestRate: (0.00416667 * 12),
+        currentMonthlyPayment: 976,
+        currentInterestRate: 0.04215,
+        loanAmount: 273224,
+        monthsRemaining: 20,
         newTerm: 15
     };
 
-    console.log(calculateNewPayment(ex.loanAmount, ex.monthlyInterestRate,
-                                    ex.newTerm));
+    populateExample(ex);
+
+    $('#current-form').on('submit', function (e) {
+        e.preventDefault();
+        var currentDetails = getCurrentDetails($('#current-form fieldset')
+                                              .serializeArray());
+        updateResultForms(currentDetails);
+    });
 });
+
+/**
+ * Populate the current form for example data.
+ *
+ * @param {object} example the example data
+ */
+function populateExample(example) {
+    var years = Math.floor(example.monthsRemaining / 12);
+    var months = example.monthsRemaining % 12;
+
+    $('#current-form #current-monthly-payment')
+        .val(example.currentMonthlyPayment);
+    $('#current-form #current-interest-rate')
+        .val(example.currentInterestRate * 100);
+    $('#current-form #remaining-balance')
+        .val(example.loanAmount);
+    $('#current-form #remaining-years').val(years);
+    $('#current-form #remaining-months').val(months);
+    $('#current-form #new-loan-terms').val(example.newTerm)
+}
+
+/**
+ * Update the result forms.
+ *
+ * @param {object} currentDetails the current details
+ */
+function updateResultForms(currentDetails) {
+    ;
+}
+
+/**
+ * Get an object representing the current loan information.
+ *
+ * @param {array} inputs the form inputs
+ *
+ * @return {object} a current loan object
+ */
+function getCurrentDetails(inputs) {
+    var details = {};
+    inputs.forEach(function (input) {
+        details[input.name] = input.value;
+    });
+
+    return {
+        currentMonthlyPayment: parseFloat(details['current-monthly-payment']),
+        currentInterestRate: parseFloat(details['current-interest-rate']) / 100,
+        loanAmount: parseFloat(details['remaining-balance']),
+        monthsRemaining: parseInt(details['remaining-months']) +
+            (parseInt(details['remaining-years']) * 12),
+        newTerm: parseInt(details['new-loan-terms'])
+    };
+}
 
 /**
  * Calculate the new mortgage payment.
