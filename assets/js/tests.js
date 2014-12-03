966f3107409b0df9b7b0e5bad45ef6d65e797140
axocomm <axocomm@gmail.com>
2014-12-03 11:59:20 -0500

    Move tests to separate file and add result checking

---
+++ assets/js/tests.js
@@ -0,0 +1,89 @@
+describe('Display value formatting', function () {
+    it('should format a percentage', function () {
+        expect(formatDisplayValue(100, 'percentage')).toBe('100.000%');
+    });
+
+    it('should format a dollar amount', function () {
+        expect(formatDisplayValue(200, 'money')).toBe('$200.00');
+    });
+
+    it('should return the input if the type is not recognised', function () {
+        expect(formatDisplayValue(1234, 'foo')).toBe(1234);
+    });
+});
+
+describe('API results', function () {
+    var testValues = [
+        {
+            term: 10,
+            lendaRate: 3.900,
+            lendaClosingCosts: 0,
+            wellsFargoRate: 4.125,
+            wellsFargoClosingCosts: 1275.00,
+            quickenRate: 4.075,
+            quickenClosingCosts: 950.00
+        },
+        {
+            term: 15,
+            lendaRate: 4.000,
+            lendaClosingCosts: 0,
+            wellsFargoRate: 4.275,
+            wellsFargoClosingCosts: 1375.00,
+            quickenRate: 4.130,
+            quickenClosingCosts: 1000.00
+        },
+        {
+            term: 20,
+            lendaRate: 4.100,
+            lendaClosingCosts: 0,
+            wellsFargoRate: 4.391,
+            wellsFargoClosingCosts: 1475.00,
+            quickenRate: 4.223,
+            quickenClosingCosts: 1050.00
+
+        },
+        {
+            term: 30,
+            lendaRate: 4.200,
+            lendaClosingCosts: 0,
+            wellsFargoRate: 4.411,
+            wellsFargoClosingCosts: 1575.00,
+            quickenRate: 4.397,
+            quickenClosingCosts: 1100.00
+        }
+    ];
+
+
+    testValues.forEach(function (test) {
+        var lendaResult, wellsFargoResult, quickenResult;
+
+        lendaResult = getLendaResult(test.term);
+        console.log(lendaResult, test.lendaRate);
+        wellsFargoResult = getWellsFargoResult(test.term);
+        quickenResult = getQuickenResult(test.term);
+
+        it('should get the correct rate for ' + test.term + ' years from the Lenda API', function () {
+            expect(lendaResult.rate).toEqual(test.lendaRate);
+        });
+
+        it('should get the correct rate for ' + test.term + ' years from the Wells Fargo API', function () {
+            expect(wellsFargoResult.rate).toEqual(test.wellsFargoRate);
+        });
+
+        it('should get the correct rate for ' + test.term + ' years from the Quicken API', function () {
+            expect(quickenResult.rate).toEqual(test.quickenRate);
+        });
+
+        it('should get the correct closing costs for ' + test.term + ' years from the Lenda API', function () {
+            expect(lendaResult.cost).toEqual(test.lendaClosingCosts);
+        });
+
+        it('should get the correct closing costs for ' + test.term + ' years from the Wells Fargo API', function () {
+            expect(wellsFargoResult.cost).toEqual(test.wellsFargoClosingCosts);
+        });
+
+        it('should get the correct closing costs for ' + test.term + ' years from the Quicken API', function () {
+            expect(quickenResult.cost).toEqual(test.quickenClosingCosts);
+        });
+    });
+});
